26218325c1631cfeb462b3af2c876027812d0df1
Omar Rosado <orosado@apspuertorico.com>
2019-09-09 15:03:10 -0400

    Modifies modal so its handled with php, not javascript like i originally intended ;

--- includes/modal.php
+++ includes/modal.php
@@ -7,31 +7,35 @@
             <h3> Add Speaker to List </h3>
             <span id="close-modal">&times;</span>
         </header>
-     
+      
 
-        <form id="add-speaker-form" action="submit.php">
+        	  
+        <form id="add-speaker-form" action="includes/success.php" method="post">
             Speaker Name: <br>
-            <input type="text" name="speaker"><br>
+            <input id="speaker" type="text" name="speaker" required><br>
             Image link: <br>
-            <input type="text" name="link"><br>
+            <input id="link" type="text" name="link" required><br>
             Event Name: </br>
-            <input type="text" name="event"><br>
+            <input id="event" type="text" name="event" required><br>
             Event Time: </br>
-            <input type="text" name="time"></br>
+            <input id="time" type="text" name="time" required></br>
             Event Location: </br>
-            <input type="text" name="location"></br>
+            <input id="location" type="text" name="location" required></br>
             Description: </br>
             <!-- <input id="event-description-cell" type="text" name="description"> <br> -->
-            <textarea id="event-description-cell" rows="6" cols="63">
-            
-            </textarea>
+            <textarea id="event-description-cell" name="description" rows="6" cols="63"></textarea> <!-- space between textarea not good --> 
 
             <!-- input button -->
             <footer>
-                <input id="add-speaker-submit" type="submit" value="Submit">
+                <input id="add-speaker-submit" name="submit" type="submit">
             </footer>
+       
             
-        </form>
+        </form> 
+
+        <?php require 'includes/addSpeaker.php' ; ?>
+
+      
   </div>
 
 </div>
