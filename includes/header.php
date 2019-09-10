5d8d7efd2104dbc798d9141aa9d6dd86d2ee5653
Omar Rosado <orosado@apspuertorico.com>
2019-09-10 14:42:38 -0400

    Adds sm screen menu & makes function for populating both menus ;

--- includes/header.php
+++ includes/header.php
@@ -1,40 +1,41 @@
+<?php
+//  fills menu area with respective items
+    function fillmenu(){
+        $menuItems = ['Wargames', 'Hacking', 'Open Source', 'Free Software', "Liscenses"] ;
+        $menuLinks = ['https://overthewire.org/wargames/', 
+                    'https://null-byte.wonderhowto.com/', 
+                    'https://opensource.com/resources/what-open-source', 
+                    'https://www.gnu.org/philosophy/free-software-intro.en.html', 
+                    'https://fosslawyers.org/permissive-foss-licenses-bsd-apache-mit/'] ; 
+
+        for($item = 0 ; $item < count($menuItems); $item++) {
+            echo "<a href='".$menuLinks[$item]."' target=_blank><li>".$menuItems[$item]."</li></a>" ;
+        }
+    } 
+?>
+
 <header>
     <section id="logo-section">
-        <a href="https://www.wired.com/2001/03/the-story-behind-tux-the-penguin/">
+        <a href="https://www.wired.com/2001/03/the-story-behind-tux-the-penguin/" target="_blank">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/TUX_G2.svg/163px-TUX_G2.svg.png" id="logo" alt="TuxNet Logo"  />
         </a>
         <h2 id="title"> TuxNet </h2>
     </section> 
-    <section id="menu-section">
-        <ul id="menu">
-        <?php
-                $menuItems = ['Wargames', 'Hacking', 'Open Source', 'Free Software', "Liscenses"] ;
-                $menuLinks = ['https://overthewire.org/wargames/', 
-                              'https://null-byte.wonderhowto.com/', 
-                              'https://opensource.com/resources/what-open-source', 
-                              'https://www.gnu.org/philosophy/free-software-intro.en.html', 
-                              'https://fosslawyers.org/permissive-foss-licenses-bsd-apache-mit/'] ; 
 
-                for($item = 0 ; $item < count($menuItems); $item++) {
-                    // echo "<a href=".$menuLinks[$item]."><li>".$menuItems[$item]."</li></a>" ;
-                    echo "<a href='".$menuLinks[$item]."' target=_blank><li>".$menuItems[$item]."</li></a>" ;
-
-                }
-            ?>
+    <div id="menu-sm">
+                   
+        <ul id="myLinks">
+            <?php fillmenu(); ?>
         </ul>
-        <section id="menu-sm">
-            <div id="myLinks">
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </section>
-
+        <a href="javascript:void(0);" onclick="toggleMenu()" >
+            <i class="fa fa-bars fa-5x" id="hamburger-menu" aria-hidden="true"></i>
+        </a>
+    </div>
+    <section id="menu-section">
+              
+        <ul id="menu">
+        <?php fillmenu() ; ?>
+        </ul>
+                
     </section>
 </header>
-
-
-    
